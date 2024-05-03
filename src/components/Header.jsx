95a4d84137e3f8eb3595b409847942724c1ac0a1
Esammohammed <essamonly111@gmail.com>
2024-05-04 00:08:21 +0300

    Add logic to header cart button to open the cart modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+
 import logo from '../assets/logo.jpg';
 import '../index.css'; // Import CSS file
+import React, {useContext,useState} from 'react'
+import { CartContext } from '../store/shopping-crt-context';
 
-export default function Header() {
+export default function Header({ handleIsModalOpen}) {
+    const {items} = useContext(CartContext)
+    let totalItems = 0
+    for (let i = 0; i < items.length; i++) {
+        totalItems += items[i].quantity
+    }
     return (
         <div id="main-header">
             <div id = "title">
                 <img src={logo} alt="logo" />
-                <h1>React Food</h1>
+                <h1>Sasa Food</h1>
             </div>
-
-            <button>Cart</button>
+            <div className='cart-icon'>
+                <button className='button' onClick={handleIsModalOpen}>Cart {totalItems}</button>
+            </div>
         </div>
     );
 }
