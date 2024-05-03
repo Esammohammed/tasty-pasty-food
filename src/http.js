7d1c88425ff98c866f7bcae914570821809fac58
Esammohammed <essamonly111@gmail.com>
2024-05-04 00:11:20 +0300

    add saveOrder function to post orders to json file

--- src/http.js
+++ src/http.js
@@ -1,6 +1,32 @@
-export default async function getFoodProducts() {
+export  async function getFoodProducts() {
     const response = await fetch('http://localhost:3000/meals');
     const resData = await response.json();
-    console.log(resData);
+
+    // adding quantity to each product = 0 
+    resData.map((product) => {
+        product.quantity = 0
+        return product
+    })
     return resData
+}
+
+export  async function saveOrder(order) {
+    console.log(order)
+    const respone = await fetch('http://localhost:3000/orders',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({order})
+    }
+    
+   
+)
+   if (await(respone.ok)) {
+    return "success" 
+   }
+    else {``
+    return "failed"
+    }
+
 }
